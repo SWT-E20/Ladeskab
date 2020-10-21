d82f14e589c8562dfb4f3911254e382bb7bbbf12
Michael Janum Davidsen <au600586@uni.au.dk>
2020-10-21 09:46:54 +0200

    updated with interfaces

---
+++ Ladeskab/Interfaces/IRfidReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ladeskab.Interfaces
+{
+    public interface IRfidReader
+    {
+
+        //event based
+    }
+}
