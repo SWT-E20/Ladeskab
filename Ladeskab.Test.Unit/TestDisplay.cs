5cdeaa5e74dca3e2827a5e8e834ad78a947ed576
Michael Janum Davidsen <au600586@uni.au.dk>
2020-10-28 11:37:43 +0100

    Tests added for print

---
+++ Ladeskab.Test.Unit/TestDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LadeSkab;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using NUnit.Framework;
+
+namespace Ladeskab.Test.Unit
+{
+    [TestFixture]
+    public class TestDisplay
+    {
+        private Display _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new Display();
+        }
+
+        [Test]
+        public void Print_TestInputValid()
+        {
+            
+        }
+    }
+}
